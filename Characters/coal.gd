2fab3e6abdd072248e7ae098a5724c041d32f5ac
will2dai4 <william.3dai@gmail.com>
2024-08-02 23:55:04 -0400

    fixing character movement (i think)

--- Characters/coal.gd
+++ Characters/coal.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
 
-@export var move_speed = 200
+@export var move_speed:float = 200
 
 
 func _physics_process(delta):
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
+	if x_movement != 0 and y_movement != 0:
+		x_movement *= sqrt(2)
+		y_movement *= sqrt(2)
+		
 	var input_direction = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
