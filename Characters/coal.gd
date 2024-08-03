dd6f3145494bb293658dcec5ab4773f43ab959a9
Fiona Cai <fiona.cai899@gmail.com>
2024-08-03 02:11:08 -0400

    add max border for characters

--- Characters/coal.gd
+++ Characters/coal.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
 @export var move_speed:float = 200
+@export var screen_size:Vector2
 
+func _ready():
+	screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -12,11 +14,11 @@ func _physics_process(delta):
 		x_movement *= sqrt(2)
 		y_movement *= sqrt(2)
 		
-	var input_direction = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
+	var input_direction = Vector2(x_movement, y_movement)
 	
 	velocity = input_direction * move_speed
-
 	move_and_slide()
+	
+	# Clamp the position to keep the character within bounds
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
